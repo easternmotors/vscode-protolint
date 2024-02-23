7841db157ffa14ec1460b01e2248f21989f1a9d9
John Remsberg <jhremsberg@gmail.com>
2024-02-23 14:28:45 -0500

    1.1.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,124 +1,33 @@
-import { execSync } from "child_process";
-import {
-  commands,
-  languages,
-  window,
-  workspace,
-  Diagnostic,
-  DiagnosticSeverity,
-  Disposable,
-  Uri,
-} from "vscode";
+import { commands, window, workspace, Disposable, OutputChannel } from 'vscode';
 
-const diagnosticCollection = languages.createDiagnosticCollection("protolint");
+import lint from './lint';
+
 let disposable: Disposable | undefined;
+let outputChannel: OutputChannel | undefined;
 
 const activate = () => {
-  // Register the protolint lint command
-  disposable = commands.registerCommand("protolint.lint", (): void => {
-    // Ensure there is an activeTextEditor and an open VSCode workspace
-    if (!window.activeTextEditor || !workspace.workspaceFolders) {
-      return;
-    }
+  outputChannel = window.createOutputChannel('Protolint', 'proto3');
+  outputChannel.appendLine('Activating Protolint extension');
 
-    // Only lint Protobuf files
-    if (
-      window.activeTextEditor.document.languageId !== "proto" &&
-      window.activeTextEditor.document.languageId !== "proto3"
-    ) {
-      return;
-    }
-
-    // Attempt to find the VSCode workspace folder's Uri
-    let workspaceFolderUri = undefined as Uri | undefined;
-    for (let workspaceFolder of workspace.workspaceFolders ?? []) {
-      if (
-        window.activeTextEditor.document.uri.fsPath.startsWith(
-          workspaceFolder.uri.fsPath
-        )
-      ) {
-        workspaceFolderUri = workspaceFolder.uri;
-      }
-    }
-
-    // Ensure a VSCode workspace folder Uri is found
-    if (!workspaceFolderUri) {
-      return undefined;
-    }
-
-    // Start building the protolint command to be executed
-    // This defaults to the protolint executable found on the user's PATH
-    let command = `protolint lint ${window.activeTextEditor.document.uri.fsPath}`;
-
-    // If a custom protolint path is passed in, use it instead of the protolint executable found on the user's PATH
-    const protolintPath = workspace
-      .getConfiguration("protolint")
-      .get<string>("path");
-    if (protolintPath) {
-      const uri = Uri.joinPath(workspaceFolderUri, protolintPath).fsPath;
-      command = `${uri} lint ${window.activeTextEditor.document.uri.fsPath}`;
-    }
-
-    try {
-      // Run the protolint command
-      execSync(command, { cwd: workspaceFolderUri.fsPath });
-    } catch ({ stderr }: any) {
-      // Parse the stderr of the protolint command to build VSCode Diagnostics
-      const diagnostics = (stderr.toString() as string)
-        .split("\n")
-        .reduce((previousValue, currentValue) => {
-          // Ensure there is an activeTextEditor
-          if (!window.activeTextEditor) {
-            return previousValue;
-          }
-
-          // Ensure the line contains content
-          if (!currentValue) {
-            return previousValue;
-          }
-
-          // Parse the line number and the message
-          const lineNumber = parseInt(
-            currentValue.split(".proto:")[1] ?? "",
-            10
-          );
-          const message = currentValue.split("] ")[1];
-
-          // Ensure the line number and message exist
-          if (isNaN(lineNumber) || !message) {
-            return previousValue;
-          }
-
-          return [
-            ...previousValue,
-            new Diagnostic(
-              window.activeTextEditor.document.lineAt(lineNumber - 1).range,
-              message,
-              DiagnosticSeverity.Error
-            ),
-          ];
-        }, [] as Diagnostic[]);
-
-      diagnosticCollection.set(
-        window.activeTextEditor.document.uri,
-        diagnostics
-      );
+  disposable = commands.registerCommand('protolint.lint', () => {
+    if (outputChannel) {
+      lint(outputChannel);
     }
   });
 
-  // Run linter when opening a new file
   window.onDidChangeActiveTextEditor(() => {
-    commands.executeCommand("protolint.lint");
+    commands.executeCommand('protolint.lint');
   });
 
-  // Run linter when saving file
   workspace.onDidSaveTextDocument(() => {
-    commands.executeCommand("protolint.lint");
+    commands.executeCommand('protolint.lint');
   });
 };
 
 const deactivate = () => {
+  outputChannel?.appendLine('Deactivating Protolint extension');
   disposable?.dispose();
-}
+  outputChannel?.dispose();
+};
 
 export { activate, deactivate };
---
+++ src/lint.ts
@@ -0,0 +1,66 @@
+import { execSync } from 'child_process';
+import { languages, window, workspace, Diagnostic, DiagnosticSeverity, OutputChannel, Uri } from 'vscode';
+
+const diagnosticCollection = languages.createDiagnosticCollection('protolint');
+
+const lint = (outputChannel: OutputChannel): void => {
+  if (
+    window.activeTextEditor &&
+    window.activeTextEditor.document.languageId === 'proto3' &&
+    workspace.workspaceFolders
+  ) {
+    const { document } = window.activeTextEditor;
+
+    let workspaceFolderUri = undefined as Uri | undefined;
+    for (let workspaceFolder of workspace.workspaceFolders ?? []) {
+      if (document.uri.fsPath.startsWith(workspaceFolder.uri.fsPath)) {
+        workspaceFolderUri = workspaceFolder.uri;
+      }
+    }
+
+    if (workspaceFolderUri) {
+      let command = `protolint lint`;
+
+      const protolintPath = workspace.getConfiguration('protolint').get<string>('path');
+      if (protolintPath) {
+        const uri = Uri.joinPath(workspaceFolderUri, protolintPath).fsPath;
+        command = `${uri} lint`;
+      }
+
+      const protolintConfigPath = workspace.getConfiguration('protolint').get<string>('configPath');
+      if (protolintConfigPath) {
+        const uri = Uri.joinPath(workspaceFolderUri, protolintConfigPath).fsPath;
+        command += ` -config_path ${uri}`;
+      }
+
+      command += ` ${document.uri.fsPath}`;
+
+      try {
+        outputChannel.appendLine(`Running protolint command: ${command}`);
+        execSync(command, { cwd: workspaceFolderUri.fsPath });
+        outputChannel.appendLine(`Protolint found no errors`);
+        diagnosticCollection.set(document.uri, []);
+      } catch ({ stderr }: any) {
+        outputChannel.appendLine(`Protolint produced output:\n${stderr.toString().trim()}`);
+        const diagnostics = (stderr.toString().trim() as string).split('\n').reduce((previousValue, currentValue) => {
+          const lineNumber = parseInt(currentValue.split('.proto:')[1] ?? '', 10);
+          const message = currentValue.split('] ')[1];
+
+          if (!isNaN(lineNumber) && message) {
+            return [
+              ...previousValue,
+              new Diagnostic(document.lineAt(lineNumber - 1).range, message, DiagnosticSeverity.Error)
+            ];
+          }
+
+          return previousValue;
+        }, [] as Diagnostic[]);
+
+        outputChannel.appendLine(`Protolint found ${diagnostics.length} errors`);
+        diagnosticCollection.set(document.uri, diagnostics);
+      }
+    }
+  }
+};
+
+export default lint;
